0f9ce65ed03f69c50ca506c2ef68d739db722971
NeilBrown <neilb@suse.de>
2010-04-09 09:18:53 +1000

    Add -fail support to --incremental
    This can be used for hot-unplug.  When a device has been remove,
    udev can call
       mdadm --incremental --fail sda
    
    and mdadm will find the array holding sda and remove sda from
    the array.
    
    Based on code from  Doug Ledford <dledford@redhat.com>
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- mdadm.c
+++ mdadm.c
@@ -774,6 +774,9 @@ int main(int argc, char *argv[])
 			devmode = 'r';
 			continue;
 		case O(MANAGE,'f'): /* set faulty */
+		case O(INCREMENTAL,'f'): /* r for incremental is taken, use f
+					  * even though we will both fail and
+					  * remove the device */
 			devmode = 'f';
 			continue;
 		case O(INCREMENTAL,'R'):
@@ -1517,6 +1520,11 @@ int main(int argc, char *argv[])
 			 ": --incremental --scan meaningless without --run.\n");
 				break;
 			}
+			if (devmode == 'f') {
+				fprintf(stderr, Name
+			 ": --incremental --scan --fail not supported.\n");
+				break;
+			}
 			rv = IncrementalScan(verbose);
 		}
 		if (!devlist) {
@@ -1531,6 +1539,10 @@ int main(int argc, char *argv[])
 			fprintf(stderr, Name
 			       ": --incremental can only handle one device.\n");
 			rv = 1;
+			break;
+		}
+		if (devmode == 'f') {
+			rv = IncrementalRemove(devlist->devname, verbose-quiet);
 			break;
 		}
 		rv = Incremental(devlist->devname, verbose-quiet, runstop,
